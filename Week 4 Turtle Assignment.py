2d91630dbc26367573b543e7f7204e45ac899473
kaic505 <kaishingc505@gmail.com>
2021-10-05 20:57:27 -0400

    Create Week 4 Turtle Assignment.py

---
+++ Week 4 Turtle Assignment.py
@@ -0,0 +1,509 @@
+import turtle
+
+colors = ['pale turquoise', 'powder blue', 'dark turquoise', 'sky blue', 'deep sky blue', 'dodger blue']
+
+turtle.goto(-250, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, 250)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, 175)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, 100)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, 25)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, -50)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, -125)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, -200)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(200, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(125, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(50, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-25, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-100, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-175, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+turtle.goto(-250, -275)
+turtle.pendown()
+for x in range(30):
+    turtle.color(colors[x % 6])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
+turtle.penup()
+
+input()
